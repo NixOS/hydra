9f7403765cb6e7f54a400506c7e02f1694152159
Eelco Dolstra <e.dolstra@tudelft.nl>
2008-10-28 12:44:36 +0000

    * Show the latest build for each job.

--- src/HydraFrontend/lib/HydraFrontend/Controller/Root.pm
+++ src/HydraFrontend/lib/HydraFrontend/Controller/Root.pm
@@ -29,7 +29,10 @@ sub getBuild {
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
     $c->stash->{template} = 'index.tt';
-    $c->stash->{builds} = [$c->model('DB::Builds')->all];
+    $c->stash->{allBuilds} = [$c->model('DB::Builds')->all];
+    # Get the latest build for each unique job.
+    # select * from builds as x where timestamp == (select max(timestamp) from builds where name == x.name);
+    $c->stash->{latestBuilds} = [$c->model('DB::Builds')->search(undef, {order_by => "name", where => "timestamp == (select max(timestamp) from builds where name == me.name)"})];
 }
 
 
@@ -44,7 +47,7 @@ sub build :Local {
 
     my $build = getBuild($c, $id);
     return error($c, "Build with ID $id doesn't exist.") if !defined $build;
-    
+
     $c->stash->{template} = 'build.tt';
     $c->stash->{build} = $build;
     $c->stash->{id} = $id;
