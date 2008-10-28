c181fc8703704a66ab0253bc7087c9a66e98e0b5
Eelco Dolstra <e.dolstra@tudelft.nl>
2008-10-28 10:18:03 +0000

    * Put info about logs / build products in the DB.

--- src/scheduler.pl
+++ src/scheduler.pl
@@ -3,12 +3,16 @@
 use strict;
 use XML::Simple;
 use DBI;
+use File::Basename;
+
+
+my $jobsFile = "../test.nix";
 
 
 my $dbh = DBI->connect("dbi:SQLite:dbname=hydra.sqlite", "", "");
 
 
-my $jobsXml = `nix-env -f ../test.nix --query --available "*" --attr-path --out-path --drv-path --meta --xml --system-filter "*"`
+my $jobsXml = `nix-env -f $jobsFile --query --available "*" --attr-path --out-path --drv-path --meta --xml --system-filter "*"`
     or die "cannot evaluate the Nix expression containing the job definitions: $?";
 
 print "$jobsXml";
@@ -20,20 +24,49 @@ my $jobs = XMLin($jobsXml, KeyAttr => ['attrPath', 'name'])
 
 foreach my $jobName (keys %{$jobs->{item}}) {
     my $job = $jobs->{item}->{$jobName};
-    print "JOB: $jobName ($job->{meta}->{description}->{value})\n";
+    my $description = defined $job->{meta}->{description} ? $job->{meta}->{description}->{value} : "";
+    print "JOB: $jobName ($description)\n";
 
     my $outPath = $job->{outPath};
+    my $drvPath = $job->{drvPath};
 
     if (scalar(@{$dbh->selectall_arrayref("select * from builds where name = ? and outPath = ?", {}, $jobName, $outPath)}) > 0) {
         print "  already done\n";
         next;
     }
 
-    my $res = system("nix-build ../test.nix --attr $jobName");
+    my $res = system("nix-build $jobsFile --attr $jobName");
 
     my $buildStatus = $res == 0 ? 0 : 1;
 
+    $dbh->begin_work;
+
     $dbh->prepare("insert into builds(timestamp, name, description, drvPath, outPath, buildStatus) values(?, ?, ?, ?, ?, ?)")
-        ->execute(time(), $jobName, $job->{meta}->{description}->{value}, $job->{drvPath}, $outPath, $buildStatus);
-    print "  db id = ", $dbh->last_insert_id(undef, undef, undef, undef), "\n";
+        ->execute(time(), $jobName, $description, $drvPath, $outPath, $buildStatus);
+    
+    my $buildId = $dbh->last_insert_id(undef, undef, undef, undef);
+    print "  db id = $buildId\n";
+
+    if ($buildStatus == 0) {
+
+        $dbh->prepare("insert into buildProducts(buildId, type, subtype, path) values(?, ?, ?, ?)")
+            ->execute($buildId, "nix-build", "", $outPath);
+        
+        my $logPath = "/nix/var/log/nix/drvs/" . basename $drvPath;
+        if (-e $logPath) {
+            print "  LOG $logPath\n";
+            $dbh->prepare("insert into buildLogs(buildId, logPhase, path, type) values(?, ?, ?, ?)")
+                ->execute($buildId, "full", $logPath, "raw");
+        }
+
+        if (-e "$outPath/log") {
+            foreach my $logPath (glob "$outPath/log/*") {
+                print "  LOG $logPath\n";
+                $dbh->prepare("insert into buildLogs(buildId, logPhase, path, type) values(?, ?, ?, ?)")
+                    ->execute($buildId, basename($logPath), $logPath, "raw");
+            }
+        }
+    }
+
+    $dbh->commit;
 }
