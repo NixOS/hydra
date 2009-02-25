537f7c8c88efdd3f1f466d58721b0a5b89cc9385
Eelco Dolstra <e.dolstra@tudelft.nl>
2009-02-25 14:33:33 +0000

    * Forgot to commit.

---
+++ src/Hydra/lib/Hydra/View/NixPkg.pm
@@ -0,0 +1,22 @@
+package Hydra::View::NixPkg;
+
+use strict;
+use base qw/Catalyst::View/;
+
+sub process {
+    my ($self, $c) = @_;
+
+    $c->response->content_type('application/nix-package');
+
+    my $build = $c->stash->{build};
+
+    my $s = "NIXPKG1 " . $c->uri_for("manifest", $build->id)
+        . " " . $build->nixname . " " . $build->system
+        . " " . $build->drvpath . " " . $build->outpath;
+    
+    $c->response->body($s);
+
+    return 1;
+}
+
+1;
