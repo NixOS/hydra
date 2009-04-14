2a05522189b166fd96928c81f747812f5c57c5af
Eelco Dolstra <e.dolstra@tudelft.nl>
2009-04-14 12:40:00 +0000

    * Don't index any of the logs (mostly because xsltproc can be a huge   resource hog).

--- src/lib/Hydra/Controller/Root.pm
+++ src/lib/Hydra/Controller/Root.pm
@@ -274,7 +274,7 @@ sub robots_txt : Path('robots.txt') {
     my @rules =
         ( uri_for('Build', 'buildtimedeps', ["*"])
         , uri_for('Build', 'runtimedeps', ["*"])
-        , uri_for('Build', 'view_nixlog', ["*"], "*/tail")
+        , uri_for('Build', 'view_nixlog', ["*"], "*")
         , channelUris('Root', ["*"])
         , channelUris('Project', ["*", "*"])
         , channelUris('Jobset', ["*", "*", "*"])
