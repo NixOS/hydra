413d49342dacc9779eeda861e53dc6f01615d0fa
Rob Vermaas <rob.vermaas@gmail.com>
2011-03-15 14:57:05 +0000

    fixed bug when updating user when no roles were selected, changed change-password link

--- src/lib/Hydra/Controller/Admin.pm
+++ src/lib/Hydra/Controller/Admin.pm
@@ -83,7 +83,7 @@ sub updateUser {
             $user->userroles->create({ role => $s}) ;
         }       
     } else {
-        $user->userroles->create({ role => $roles}) ;
+        $user->userroles->create({ role => $roles}) if defined $roles ;
     }   
 }
 
@@ -158,7 +158,7 @@ sub reset_password : Chained('user') PathPart('reset-password') Args(0) {
         "New password for Hydra",
         "Hi,\n\n".
         "Your password has been reset. Your new password is '$password'.\n".
-        "You can change your password at http://".hostname_long." .\n".
+        "You can change your password at http://".hostname_long."/change-password .\n".
         "With regards, Hydra\n"
     );
 
