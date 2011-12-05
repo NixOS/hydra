09e26a301174ba38b7972e16953ba856dc949145
Eelco Dolstra <e.dolstra@tudelft.nl>
2011-12-05 15:21:43 +0100

    Fix broken test

--- tests/query-all-tables.pl
+++ tests/query-all-tables.pl
@@ -7,15 +7,17 @@ my $db = openHydraDB;
 my @sources = $db->sources;
 my $nrtables = scalar(@sources);
 
-use Test::Simple tests => 43;
+use Test::Simple tests => 44;
 
 foreach my $source (@sources) {
   my $title = "Basic select query for $source";
-  if( $source !~ m/^(LatestSucceeded|JobStatus|ActiveJobs)/) {
-    ok(scalar($db->resultset($source)->all) == 0, $title);
+  if ($source eq "SchemaVersion") {
+      ok(scalar($db->resultset($source)->all) == 1, $title);
+  } elsif( $source !~ m/^(LatestSucceeded|JobStatus|ActiveJobs)/) {
+      ok(scalar($db->resultset($source)->all) == 0, $title);
   } 
   else {
-    ok(scalar($db->resultset($source)->search({},{ bind => ["", "", ""] })) == 0, $title);
+      ok(scalar($db->resultset($source)->search({},{ bind => ["", "", ""] })) == 0, $title);
   }
 }
 
