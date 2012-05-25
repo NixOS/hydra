fdf441a8b72b7b3270144b0a9fcd061266d73f3c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2012-05-25 14:27:56 +0000

    Set utf-8 encoding on raw log pages

--- src/lib/Hydra/View/Plain.pm
+++ src/lib/Hydra/View/Plain.pm
@@ -4,4 +4,10 @@ use strict;
 use warnings;
 use base 'Catalyst::View::Download::Plain';
 
+sub process {
+    my ($self, $c) = @_;
+    $c->res->content_encoding("utf-8");
+    $self->SUPER::process($c);
+}
+
 1;
