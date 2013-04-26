71eaa115f831e872be84851ef48d88eec9c57329
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-04-26 13:23:34 +0200

    Ellipsize long jobset/job names to prevent bootstrap navbar breakage

--- src/lib/Hydra/View/TT.pm
+++ src/lib/Hydra/View/TT.pm
@@ -8,12 +8,17 @@ __PACKAGE__->config(
     TEMPLATE_EXTENSION => '.tt',
     PRE_CHOMP => 1,
     POST_CHOMP => 1,
-    expose_methods => [qw/log_exists/]);
+    expose_methods => [qw/log_exists ellipsize/]);
 
 sub log_exists {
     my ($self, $c, $drvPath) = @_;
     my $x = getDrvLogPath($drvPath);
     return defined $x;
+}
+
+sub ellipsize {
+    my ($self, $c, $s, $n) = @_;
+    return length $s <= $n ? $s : substr($s, 0, $n - 3) . "...";
 }
 
 1;
