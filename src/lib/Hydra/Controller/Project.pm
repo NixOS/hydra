1404d33005622a4741b6d79480995ff6d29b6c30
Shea Levy <shea@shealevy.com>
2013-07-25 17:48:28 -0400

    Show when a Project's jobsets are disabled
    Signed-off-by: Shea Levy <shea@shealevy.com>

--- src/lib/Hydra/Controller/Project.pm
+++ src/lib/Hydra/Controller/Project.pm
@@ -23,6 +23,7 @@ sub projectChain :Chained('/') :PathPart('project') :CaptureArgs(1) {
       "releases.name",
       "releases.timestamp",
       "jobsets.name",
+      "jobsets.disabled",
     ], join => [ 'owner', 'views', 'releases', 'jobsets' ], order_by => { -desc => "releases.timestamp" }, collapse => 1 });
 
     if ($project) {
