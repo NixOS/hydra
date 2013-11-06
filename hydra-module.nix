9dc3311ff7c3c81bc6391a3bca3694dd06d26a73
Eelco Dolstra <eelco.dolstra@logicblox.com>
2013-11-06 16:15:53 +0100

    hydra-module.nix: Don't create an admin user
    It's the user's job to create users with the appropriate roles.

--- hydra-module.nix
+++ hydra-module.nix
@@ -194,12 +194,6 @@ in
               mv ${baseDir}/.pgpass-tmp ${baseDir}/.pgpass
           fi
           ${pkgs.shadow}/bin/su hydra -c ${cfg.hydra}/bin/hydra-init
-          ${config.services.postgresql.package}/bin/psql hydra << EOF
-            BEGIN;
-            INSERT INTO Users(userName, emailAddress, password) VALUES ('admin', '${cfg.notificationSender}', '$(echo -n $pass | sha1sum | cut -c1-40)');
-            INSERT INTO UserRoles(userName, role) values('admin', 'admin');
-            COMMIT;
-          EOF
         '';
         serviceConfig.Type = "oneshot";
         serviceConfig.RemainAfterExit = true;
