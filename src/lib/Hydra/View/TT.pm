c6d504edbb7d3ff808a20bcc2c87046106f1c84a
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-06-17 13:49:18 +0200

    Handle SSH hosts without a @

--- src/lib/Hydra/View/TT.pm
+++ src/lib/Hydra/View/TT.pm
@@ -9,7 +9,7 @@ __PACKAGE__->config(
     ENCODING => 'utf-8',
     PRE_CHOMP => 1,
     POST_CHOMP => 1,
-    expose_methods => [qw/buildLogExists buildStepLogExists jobExists/]);
+    expose_methods => [qw/buildLogExists buildStepLogExists jobExists stripSSHUser/]);
 
 sub buildLogExists {
     my ($self, $c, $build) = @_;
@@ -21,6 +21,16 @@ sub buildStepLogExists {
     my ($self, $c, $step) = @_;
     my @outPaths = map { $_->path } $step->buildstepoutputs->all;
     return defined findLog($c, $step->drvpath, @outPaths);
+}
+
+
+sub stripSSHUser {
+    my ($self, $c, $name) = @_;
+    if ($name =~ /^.*@(.*)$/) {
+        return $1;
+    } else {
+        return $name;
+    }
 }
 
 # Check whether the given job is a member of the most recent jobset
