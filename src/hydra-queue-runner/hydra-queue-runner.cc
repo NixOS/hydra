f21b88e3888289428ef27b8787f793f14bf11981
Eelco Dolstra <eelco.dolstra@logicblox.com>
2015-08-07 04:20:34 +0200

    Remove superfluous check

--- src/hydra-queue-runner/hydra-queue-runner.cc
+++ src/hydra-queue-runner/hydra-queue-runner.cc
@@ -214,7 +214,7 @@ void State::markSucceededBuild(pqxx::work & txn, Build::ptr build,
     if (txn.parameterized("select 1 from Builds where id = $1 and finished = 0")(build->id).exec().empty()) return;
 
     txn.parameterized
-        ("update Builds set finished = 1, busy = 0, buildStatus = $2, startTime = $3, stopTime = $4, size = $5, closureSize = $6, releaseName = $7, isCachedBuild = $8 where id = $1 and finished = 0")
+        ("update Builds set finished = 1, busy = 0, buildStatus = $2, startTime = $3, stopTime = $4, size = $5, closureSize = $6, releaseName = $7, isCachedBuild = $8 where id = $1")
         (build->id)
         ((int) (res.failed ? bsFailedWithOutput : bsSuccess))
         (startTime)
