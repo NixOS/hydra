NixOS/hydra

commit 1fdc258de0d7f97af428de34e8be019fca3db251

ListBuilds: Simplify nix channel dispatcher.
We only allow channel/latest anyway, so it really doesn't make sense to
explicitly specify this in the PathPart and provide other dispatcher
once we have more than just "latest", which greatly simplifies the
dispatch tree.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

src/lib/Hydra/Base/Controller/ListBuilds.pm
@@ -32,23 +32,16 @@ sub all : Chained('get_builds') PathPart {
 }
 
 
-sub nix : Chained('get_builds') PathPart('channel') CaptureArgs(1) {
+sub nix : Chained('get_builds') PathPart('channel/latest') CaptureArgs(1) {
     my ($self, $c, $channelName) = @_;
-    eval {
-        if ($channelName eq "latest") {
-            $c->stash->{channelName} = $c->stash->{channelBaseName} . "-latest";
-            $c->stash->{channelBuilds} = $c->stash->{latestSucceeded}
-                ->search_literal("exists (select 1 from buildproducts where build = me.id and type = 'nix-build')")
-                ->search({}, { columns => [@buildListColumns, 'drvpath', 'description', 'homepage']
-                             , join => ["buildoutputs"]
-                             , order_by => ["me.id", "buildoutputs.name"]
-                             , '+select' => ['buildoutputs.path', 'buildoutputs.name'], '+as' => ['outpath', 'outname'] });
-        }
-        else {
-            notFound($c, "Unknown channel `$channelName'.");
-        }
-    };
-    error($c, $@) if $@;
+
+    $c->stash->{channelName} = $c->stash->{channelBaseName} . "-latest";
+    $c->stash->{channelBuilds} = $c->stash->{latestSucceeded}
+        ->search_literal("exists (select 1 from buildproducts where build = me.id and type = 'nix-build')")
+        ->search({}, { columns => [@buildListColumns, 'drvpath', 'description', 'homepage']
+                     , join => ["buildoutputs"]
+                     , order_by => ["me.id", "buildoutputs.name"]
+                     , '+select' => ['buildoutputs.path', 'buildoutputs.name'], '+as' => ['outpath', 'outname'] });
 }
 
 

commit e04f5eab6602f0e7881b90a9a38605636cee47b6

ListBuilds: Fix legacy channel URLs.
Regression introduced by 1fdc258de0d7f97af428de34e8be019fca3db251.

The commit introduced a channel/custom PathPart which uses the new
custom channel expressions, but I forgot to remove CaptureArgs, so the
URL really is channel/latest/ignored-value.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>
Reported-by: Peter Simons <simons@cryp.to>

src/lib/Hydra/Base/Controller/ListBuilds.pm
@@ -32,8 +32,8 @@ sub all : Chained('get_builds') PathPart {
 }
 
 
-sub nix : Chained('get_builds') PathPart('channel/latest') CaptureArgs(1) {
-    my ($self, $c, $channelName) = @_;
+sub nix : Chained('get_builds') PathPart('channel/latest') {
+    my ($self, $c) = @_;
 
     $c->stash->{channelName} = $c->stash->{channelBaseName} . "-latest";
     $c->stash->{channelBuilds} = $c->stash->{latestSucceeded}