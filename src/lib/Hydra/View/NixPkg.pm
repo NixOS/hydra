e45bbfbef068e61567760efd8769bff7a9431699
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-03-02 15:35:55 +0100

    Fix .nixpkg channel uri
    Fixes #274.

--- src/lib/Hydra/View/NixPkg.pm
+++ src/lib/Hydra/View/NixPkg.pm
@@ -10,11 +10,18 @@ sub process {
 
     my $build = $c->stash->{build};
 
+    my $storeMode = $c->config->{store_mode} // "direct";
+    my $channelUri =
+        $storeMode eq "direct" ? $c->uri_for('/')
+        : $storeMode eq "s3-binary-cache" ?
+          ($c->config->{binary_cache_public_uri} // ("https://" . $c->config->{binary_cache_s3_bucket} . ".s3.amazonaws.com/"))
+        : die "Not supported.\n";
+
     # FIXME: add multiple output support
     my $s = "NIXPKG1 http://invalid.org/"
         . " " . $build->nixname . " " . $build->system
         . " " . $build->drvpath . " " . $build->buildoutputs->find({name => "out"})->path
-        . " " . $c->uri_for('/');
+        . " " . $channelUri;
 
     $c->response->body($s);
 
