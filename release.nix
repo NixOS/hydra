e624652dd8b762bacf7c58b5595ce919aca392d2
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-03-22 13:11:30 +0100

    Use patched aws-sdk-cpp

--- release.nix
+++ release.nix
@@ -120,10 +120,17 @@ rec {
           guile # optional, for Guile + Guix support
           perlDeps perl
           postgresql92 # for running the tests
-          (aws-sdk-cpp.override {
+          (lib.overrideDerivation (aws-sdk-cpp.override {
             apis = ["s3"];
             customMemoryManagement = false;
-          })
+          }) (attrs: {
+            src = fetchFromGitHub {
+              owner = "edolstra";
+              repo = "aws-sdk-cpp";
+              rev = "local";
+              sha256 = "1vhgsxkhpai9a7dk38q4r239l6dsz2jvl8hii24c194lsga3g84h";
+            };
+          }))
         ];
 
       hydraPath = lib.makeSearchPath "bin" (
