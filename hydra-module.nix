32fa3921463cd7688b6b61602cfc707f406b46ae
Eelco Dolstra <eelco.dolstra@logicblox.com>
2016-03-23 12:35:55 +0100

    Fix hydra-queue-runner PATH

--- hydra-module.nix
+++ hydra-module.nix
@@ -284,7 +284,7 @@ in
       { wantedBy = [ "multi-user.target" ];
         requires = [ "hydra-init.service" ];
         after = [ "hydra-init.service" "network.target" ];
-        path = [ pkgs.nettools pkgs.ssmtp ];
+        path = [ cfg.package pkgs.nettools pkgs.ssmtp pkgs.openssh pkgs.bzip2 ];
         environment = env // {
           PGPASSFILE = "${baseDir}/pgpass-queue-runner"; # grrr
           IN_SYSTEMD = "1"; # to get log severity levels
