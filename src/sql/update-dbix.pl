ee30978f03e2dbfda2ed47e2b06522c710d464e1
aszlig <aszlig@redmoonstudios.org>
2016-07-06 00:40:33 +0200

    update-dbix: Switch moniker_map to use new names
    We want to map underscore-separated tables to CamelCase so that we still
    have the same class names as we had so far.
    
    Unfortunately we can't use the default moniker, because it tries to make
    pluralized names singular.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- src/sql/update-dbix.pl
+++ src/sql/update-dbix.pl
@@ -1,7 +1,10 @@
 make_schema_at("Hydra::Schema", {
     naming => { ALL => "v5" },
     relationships => 1,
-    moniker_map => sub { return "$_"; },
+    moniker_map => sub {
+        return "CachedCVSInputs" if $_ eq "cached_cvs_inputs";
+        return $_ =~ s/(?:_|^)([a-z])/\u$1/gr;
+    },
     components => [ "+Hydra::Component::ToJSON" ],
     rel_name_map => { buildsteps_builds => "buildsteps" }
 }, [$ENV{"HYDRA_DBI"}]);
