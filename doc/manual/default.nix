b67255fe1ca22ba949b9fc7a1a759708ffbfb4f5
Eric Sagnes <eric.sagnes@gmail.com>
2016-07-29 20:01:06 +0900

    doc: add pdf manual

--- doc/manual/default.nix
+++ doc/manual/default.nix
@@ -3,51 +3,72 @@ let
   lib = pkgs.lib;
   sources = lib.sourceFilesBySuffices ./. [".xml"];
 in
-pkgs.stdenv.mkDerivation {
-  name = "hydra-manual";
+{
+  manual = pkgs.stdenv.mkDerivation {
+    name = "hydra-manual";
 
-  buildInputs = with pkgs; [ pandoc libxml2 libxslt zip ];
+    buildInputs = with pkgs; [ libxml2 libxslt ];
 
-  xsltFlags = ''
-    --param section.autolabel 1
-    --param section.label.includes.component.label 1
-    --param html.stylesheet 'style.css'
-    --param xref.with.number.and.title 1
-    --param toc.section.depth 3
-    --param admon.style '''
-    --param callout.graphics.extension '.gif'
-  '';
+    xsltFlags = ''
+      --param section.autolabel 1
+      --param section.label.includes.component.label 1
+      --param html.stylesheet 'style.css'
+      --param xref.with.number.and.title 1
+      --param toc.section.depth 3
+      --param admon.style '''
+      --param callout.graphics.extension '.gif'
+    '';
 
-  buildCommand = ''
-    ln -s '${sources}/'*.xml .
+    buildCommand = ''
+      ln -s '${sources}/'*.xml .
 
-    # validate against relaxng schema
-    xmllint --nonet --xinclude --noxincludenode manual.xml --output manual-full.xml
-    ${pkgs.jing}/bin/jing ${pkgs.docbook5}/xml/rng/docbook/docbook.rng manual-full.xml
+      # validate against relaxng schema
+      xmllint --nonet --xinclude --noxincludenode manual.xml --output manual-full.xml
+      ${pkgs.jing}/bin/jing ${pkgs.docbook5}/xml/rng/docbook/docbook.rng manual-full.xml
 
-    dst=$out/share/doc/hydra
-    mkdir -p $dst
-    xsltproc $xsltFlags --nonet --xinclude \
-      --output $dst/manual.html \
-      ${pkgs.docbook5_xsl}/xml/xsl/docbook/xhtml/docbook.xsl \
-      ./manual.xml
+      dst=$out/share/doc/hydra
+      mkdir -p $dst
+      xsltproc $xsltFlags --nonet --xinclude \
+        --output $dst/manual.html \
+        ${pkgs.docbook5_xsl}/xml/xsl/docbook/xhtml/docbook.xsl \
+        ./manual.xml
 
-    cp ${./style.css} $dst/style.css
+      cp ${./style.css} $dst/style.css
 
-    mkdir -p $dst/images/callouts
-    cp "${pkgs.docbook5_xsl}/xml/xsl/docbook/images/callouts/"*.gif $dst/images/callouts/
+      mkdir -p $dst/images/callouts
+      cp "${pkgs.docbook5_xsl}/xml/xsl/docbook/images/callouts/"*.gif $dst/images/callouts/
 
-    mkdir -p $out/nix-support
-    echo "doc manual $dst manual.html" >> $out/nix-support/hydra-build-products
+      mkdir -p $out/nix-support
+      echo "doc manual $dst manual.html" >> $out/nix-support/hydra-build-products
+    '';
+  };
 
-    xsltproc $xsltFlags --nonet --xinclude \
-      --output $dst/epub/ \
-      ${pkgs.docbook5_xsl}/xml/xsl/docbook/epub/docbook.xsl \
-      ./manual.xml
+  manualPDF = pkgs.stdenv.mkDerivation {
+    name = "hydra-manual-pdf";
 
-    cp -r $dst/images $dst/epub/OEBPS
-    echo "application/epub+zip" > mimetype
-    zip -0Xq  "$dst/Hydra User's Guide - NixOS community.epub" mimetype
-    zip -Xr9D "$dst/Hydra User's Guide - NixOS community.epub" $dst/epub/*
-  '';
+    buildInputs = with pkgs; [ libxml2 libxslt dblatex dblatex.tex ];
+
+    xsltFlags = ''
+      --param section.autolabel 1
+      --param section.label.includes.component.label 1
+      --param html.stylesheet 'style.css'
+      --param xref.with.number.and.title 1
+      --param toc.section.depth 3
+      --param admon.style '''
+      --param callout.graphics.extension '.gif'
+    '';
+
+    buildCommand = ''
+      ln -s '${sources}/'*.xml .
+ 
+      dst=$out/share/doc/nixos
+      mkdir -p $dst
+      xmllint --xinclude manual.xml | dblatex -o $dst/manual.pdf - \
+        -P doc.collab.show=0 \
+        -P latex.output.revhistory=0
+
+      mkdir -p $out/nix-support
+      echo "doc-pdf manual $dst/manual.pdf" >> $out/nix-support/hydra-build-products
+    '';
+  };
 }
