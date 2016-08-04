64cddfa4be5da24c691d65830ea1a6eca6389fcf
aszlig <aszlig@redmoonstudios.org>
2016-08-04 22:04:07 +0200

    Plugin: Finish/fix documentation for properties
    The documentation so far was quite wrong, because it was describing one
    of my experiments during the design of the property system.
    
    Now the documentation should be up to date and will hopefully be helpful
    enough to understand the system.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- src/lib/Hydra/Plugin.pm
+++ src/lib/Hydra/Plugin.pm
@@ -39,20 +39,68 @@ sub stepFinished {
     my ($self, $step, $logPath) = @_;
 }
 
-# Called to determine the set of supported input types.  The plugin
-# should add these to the $inputTypes hashref passed to the function.
+# Called to determine the set of supported input types. The plugin should add
+# these to the $inputTypes hashref passed to the function.
 #
-# The value of it is another hashref, which defines some properties for
-# handling that input type, like how to render the types properties or
-# how to validate them.
+# The value of it is another hashref, which defines some properties for handling
+# that input type, like how to render the types properties or how to validate
+# them.
 #
 # For example:
 #
 #   $inputTypes{'svn'} = {
 #     name => 'Subversion checkout',
-#     render => sub { ... },
-#     validate => sub { ... },
+#     properties => {
+#       uri => {label => "URI", required => 1},
+#       revision => {label => "Revision"},
+#     }
 #   };
+#
+# The top-level of this consists of "name" (which is the user-visible name of
+# the input type) and either "properties" or "singleton".
+#
+# The "properties" hashref is a collection of fields that can be set for a
+# particular input and it maps from the name to another hashref specifying the
+# information about that property:
+#
+#   label:    The label that is used for printing/editing
+#   type:     The type of this property (for available types see below)
+#   required: Whether the user is required to enter a value for this property
+#   help:     A help text that is displayed as a tool tip
+#   validate: A function that is used to validate the input (details below)
+#
+# All of these attributes are supported by "singleton" as well, because instead
+# of a list of properties the "singleton" attribute designates only one possible
+# input.
+#
+# Available property types:
+#
+#   bool:    Boolean
+#   int:     Integer
+#   attrset: A map between keys and values
+#   string:  String
+#
+# If no property type is specified, the default is "string".
+#
+# The "validate" property attribute is a function which gets passed the
+# following arguments in this order:
+#
+#   controller: The current Controller object
+#   name:       The name of the jobset input
+#   value:      The value of the property
+#
+# The "controller" argument is very useful to trigger errors, like for example:
+#
+#   myproperty => {
+#     label    => "An example property",
+#     validate => sub {
+#       my ($c, $name, $value) = @_;
+#       error($c, 'You have entered an evil value!') if $value eq "evil";
+#     }
+#   }
+#
+# In this case if "evil" is entered as the properties value, an error would be
+# returned and displayed to the user.
 #
 sub supportedInputTypes {
     my ($self, $inputTypes) = @_;
