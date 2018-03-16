4015bca225eddcee70b3f17e947b7e631fee8e7e
Michael Bishop <cleverca22@gmail.com>
2018-03-16 13:04:48 -0300

    fix a stray newline in a path

--- src/lib/Hydra/Plugin/GithubPulls.pm
+++ src/lib/Hydra/Plugin/GithubPulls.pm
@@ -58,6 +58,7 @@ sub fetchInput {
     system("jq -S . < $filename > $tempdir/github-pulls-sorted.json");
     my $storePath = trim(`nix-store --add "$tempdir/github-pulls-sorted.json"`
         or die "cannot copy path $filename to the Nix store.\n");
+    chomp $storePath;
     my $timestamp = time;
     return { storePath => $storePath, revision => strftime "%Y%m%d%H%M%S", gmtime($timestamp) };
 }
