f55e15cad575f660d5c385c7720b3167fc7bccb2
Matthew Ray <matthew.ray@infor.com>
2019-04-01 15:51:22 -0400

    Move some more common operations to util class

--- src/lib/Hydra/Helper/Notification.pm
+++ src/lib/Hydra/Helper/Notification.pm
@@ -2,9 +2,18 @@ package Hydra::Helper::Notification;
 
 use strict;
 use Exporter;
+use HTTP::Request;
+use LWP::UserAgent;
+use Hydra::Helper::CatalystUtils;
+use JSON;
 
 our @ISA = qw(Exporter);
-our @EXPORT = qw(renderDuration);
+our @EXPORT = qw(
+    renderDuration
+    getChannelsToNotify
+    selectImage
+    selectColor
+    sendMessage);
 
 sub renderDuration {
     my ($build) = @_;
@@ -21,6 +30,72 @@ sub renderDuration {
     }
     $res .= ($duration % 60) . "s";
     return $res;
+}
+
+
+sub getChannelsToNotify {
+    my ($build, $dependents, $cfg) = @_;
+
+    my @config = defined $cfg ? ref $cfg eq "ARRAY" ? @$cfg : ($cfg) : ();
+
+    # Figure out to which channelss to send notification.  For each channel
+    # we send one aggregate message.
+    my %channels;
+    foreach my $b ($build, @{$dependents}) {
+        my $prevBuild = getPreviousBuild($b);
+        my $jobName = showJobName $b;
+
+        foreach my $channel (@config) {
+            my $force = $channel->{force};
+            next unless $jobName =~ /^$channel->{jobs}$/;
+
+            # If build is cancelled or aborted, do not send email.
+            next if ! $force && ($b->buildstatus == 4 || $b->buildstatus == 3);
+
+            # If there is a previous (that is not cancelled or aborted) build
+            # with same buildstatus, do not send email.
+            next if ! $force && defined $prevBuild && ($b->buildstatus == $prevBuild->buildstatus);
+
+            $channels{$channel->{url}} //= { channel => $channel, builds => [] };
+            push @{$channels{$channel->{url}}->{builds}}, $b;
+        }
+    }
+
+    return %channels;
+}
+
+sub selectImage {
+    my ($build) = @_;
+
+    my $imgBase = "http://hydra.nixos.org";
+    my $img =
+        $build->buildstatus == 0 ? "$imgBase/static/images/checkmark_256.png" :
+        $build->buildstatus == 2 ? "$imgBase/static/images/dependency_256.png" :
+        $build->buildstatus == 4 ? "$imgBase/static/images/cancelled_128.png" :
+        "$imgBase/static/images/error_256.png";
+
+    return $img;
+}
+
+sub selectColor {
+    my ($build) = @_;
+
+    my $color =
+        $build->buildstatus == 0 ? "good" :
+        $build->buildstatus == 4 ? "warning" :
+        "danger";
+
+    return $color;
+}
+
+sub sendMessage {
+    my ($url, $msg) = @_;
+
+    my $req = HTTP::Request->new('POST', $url);
+    $req->header('Content-Type' => 'application/json');
+    $req->content(encode_json($msg));
+    my $ua = LWP::UserAgent->new();
+    $ua->request($req);
 }
 
 1;
--- src/lib/Hydra/Plugin/MicrosoftTeamsNotification.pm
+++ src/lib/Hydra/Plugin/MicrosoftTeamsNotification.pm
@@ -2,8 +2,6 @@ package Hydra::Plugin::MicrosoftTeamsNotification;
 
 use strict;
 use parent 'Hydra::Plugin';
-use HTTP::Request;
-use LWP::UserAgent;
 use Hydra::Helper::CatalystUtils;
 use Hydra::Helper::Notification;
 use JSON;
@@ -40,33 +38,12 @@ sub createMessageJSON {
 sub buildFinished {
     my ($self, $build, $dependents) = @_;
     my $cfg = $self->{config}->{slack};
-    my @config = defined $cfg ? ref $cfg eq "ARRAY" ? @$cfg : ($cfg) : ();
 
     my $baseurl = $self->{config}->{'base_uri'} || "http://localhost:3000";
 
     # Figure out to which channelss to send notification.  For each channel
     # we send one aggregate message.
-    my %channels;
-    foreach my $b ($build, @{$dependents}) {
-        my $prevBuild = getPreviousBuild($b);
-        my $jobName = showJobName $b;
-
-        foreach my $channel (@config) {
-            my $force = $channel->{force};
-            next unless $jobName =~ /^$channel->{jobs}$/;
-
-            # If build is cancelled or aborted, do not send email.
-            next if ! $force && ($b->buildstatus == 4 || $b->buildstatus == 3);
-
-            # If there is a previous (that is not cancelled or aborted) build
-            # with same buildstatus, do not send email.
-            next if ! $force && defined $prevBuild && ($b->buildstatus == $prevBuild->buildstatus);
-
-            $channels{$channel->{url}} //= { channel => $channel, builds => [] };
-            push @{$channels{$channel->{url}}->{builds}}, $b;
-        }
-    }
-
+    my %channels = getChannelsToNotify($build, $dependents, $cfg);
     return if scalar keys %channels == 0;
 
     my ($authors, $nrCommits) = getResponsibleAuthors($build, $self->{plugins});
@@ -76,17 +53,8 @@ sub buildFinished {
         my $channel = $channels{$url};
         my @deps = grep { $_->id != $build->id } @{$channel->{builds}};
 
-        my $imgBase = "http://hydra.nixos.org";
-        my $img =
-            $build->buildstatus == 0 ? "$imgBase/static/images/checkmark_256.png" :
-            $build->buildstatus == 2 ? "$imgBase/static/images/dependency_256.png" :
-            $build->buildstatus == 4 ? "$imgBase/static/images/cancelled_128.png" :
-            "$imgBase/static/images/error_256.png";
-
-        my $color =
-            $build->buildstatus == 0 ? "good" :
-            $build->buildstatus == 4 ? "warning" :
-            "danger";
+        my $img = selectImage($build);
+        my $color = selectColor($build);
 
         my $text = "";
         $text .= "Job " . createTextLink("$baseurl/job/${\$build->project->name}/${\$build->jobset->name}/${\$build->job->name}", showJobName($build));
@@ -103,11 +71,7 @@ sub buildFinished {
 
         my $msg = createMessageJSON($baseurl, $build, $text, $img, $color);
 
-        my $req = HTTP::Request->new('POST', $url);
-        $req->header('Content-Type' => 'application/json');
-        $req->content(encode_json($msg));
-        my $ua = LWP::UserAgent->new();
-        $ua->request($req);
+        sendMessage($url, $msg);
     }
 }
 
