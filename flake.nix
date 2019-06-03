bc11ab22a3654c09fc956114efe471cc40f98fc2
Eelco Dolstra <edolstra@gmail.com>
2019-06-03 12:09:18 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,15 +5,15 @@
 
   epoch = 2019;
 
-  requires = [ "nixpkgs" "nix" ];
+  inputs = [ "nixpkgs" "nix" ];
 
-  provides = deps: rec {
+  outputs = inputs: rec {
 
     hydraJobs = import ./release.nix {
-      hydraSrc = deps.self;
-      nixpkgs = deps.nixpkgs;
-      nix = deps.nix.provides.hydraJobs.build.x86_64-linux // {
-        perl-bindings = deps.nix.provides.hydraJobs.perlBindings.x86_64-linux;
+      hydraSrc = inputs.self;
+      nixpkgs = inputs.nixpkgs;
+      nix = inputs.nix.outputs.hydraJobs.build.x86_64-linux // {
+        perl-bindings = inputs.nix.outputs.hydraJobs.perlBindings.x86_64-linux;
       };
     };
 
@@ -22,8 +22,8 @@
     defaultPackage = packages.hydra;
 
     devShell = (import ./release.nix {
-      hydraSrc = deps.self;
-      nixpkgs = deps.nixpkgs;
+      hydraSrc = inputs.self;
+      nixpkgs = inputs.nixpkgs;
       shell = true;
     }).build.x86_64-linux;
 
