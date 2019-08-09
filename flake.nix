1c9e22e73c402a311a456fc559eb3ea1286aaabe
Eelco Dolstra <edolstra@gmail.com>
2019-08-09 13:10:50 +0200

    Use nixFlakes in devShell

--- flake.nix
+++ flake.nix
@@ -5,30 +5,37 @@
 
   epoch = 201906;
 
-  inputs = [ "nixpkgs" "nix" ];
+  inputs =
+    [ "nixpkgs"
+      "nix/ab16b3d076e9cd3ecfdcde128f43dd486b072557"
+    ];
 
-  outputs = inputs: rec {
-
-    hydraJobs = import ./release.nix {
-      hydraSrc = inputs.self;
-      nixpkgs = inputs.nixpkgs;
+  outputs = inputs:
+    let
       nix = inputs.nix.outputs.hydraJobs.build.x86_64-linux // {
         perl-bindings = inputs.nix.outputs.hydraJobs.perlBindings.x86_64-linux;
       };
+    in rec {
+
+      hydraJobs = import ./release.nix {
+        hydraSrc = inputs.self;
+        nixpkgs = inputs.nixpkgs;
+        inherit nix;
+      };
+
+      checks.build = hydraJobs.build.x86_64-linux;
+      checks.install = hydraJobs.tests.install.x86_64-linux;
+
+      packages.hydra = hydraJobs.build.x86_64-linux;
+
+      defaultPackage = packages.hydra;
+
+      devShell = (import ./release.nix {
+        hydraSrc = inputs.self;
+        nixpkgs = inputs.nixpkgs;
+        shell = true;
+        inherit nix;
+      }).build.x86_64-linux;
+
     };
-
-    checks.build = hydraJobs.build.x86_64-linux;
-    checks.install = hydraJobs.tests.install.x86_64-linux;
-
-    packages.hydra = hydraJobs.build.x86_64-linux;
-
-    defaultPackage = packages.hydra;
-
-    devShell = (import ./release.nix {
-      hydraSrc = inputs.self;
-      nixpkgs = inputs.nixpkgs;
-      shell = true;
-    }).build.x86_64-linux;
-
-  };
 }
