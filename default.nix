9d80a6de6653bd55977abd1b3a8aea14224979c5
Tobias Pflug <tobias.pflug@gmail.com>
2020-05-02 18:56:30 +0200

    Add comment about edolstra/flake-compat
    Add a comment to default.nix and shell.nix briefly outlining what
    flake-compat/default.nix does.

--- default.nix
+++ default.nix
@@ -1,3 +1,6 @@
+# The `default.nix` in flake-compat reads `flake.nix` and `flake.lock` from `src` and
+# returns an attribute set of the shape `{ defaultNix, shellNix }`
+
 (import (fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
   src = builtins.fetchGit ./.;
 }).defaultNix
