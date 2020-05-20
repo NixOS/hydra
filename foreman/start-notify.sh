8a169ffa56b3e6bf5316e140a42d8e0cda1c8862
Casey Ransom <casey@dailykos.com>
2020-05-20 15:32:41 -0400

    Add hydra-notify to devshell
    I came across https://github.com/NixOS/hydra/issues/751 and realized
    that hydra-notify is responsible for creating the additional jobsets in
    a declarative file. My declarative testing works in dev now.

---
+++ foreman/start-notify.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# wait for hydra-server to listen
+while ! nc -z localhost 63333; do sleep 1; done
+
+touch .hydra-data/hydra.conf
+HYDRA_CONFIG=$(pwd)/.hydra-data/hydra.conf exec hydra-notify
