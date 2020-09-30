df3262e96cb55bdfaac7726896728bfef675698b
Nathan van Doorn <nathan@myrtlesoftware.com>
2020-09-30 10:30:24 +0100

    Remove deprecation notice for binary_cache_secret_key_file
    It's still used for signing `narinfo` files if using Hydra as a binary cache server. See https://github.com/NixOS/hydra/blob/master/src/lib/Hydra/View/NARInfo.pm#L37

--- src/hydra-queue-runner/hydra-queue-runner.cc
+++ src/hydra-queue-runner/hydra-queue-runner.cc
@@ -57,8 +57,6 @@ State::State()
         printMsg(lvlError, "hydra.conf: binary_cache_dir is deprecated and ignored. use store_uri=file:// instead");
     if (config->getStrOption("binary_cache_s3_bucket") != "")
         printMsg(lvlError, "hydra.conf: binary_cache_s3_bucket is deprecated and ignored. use store_uri=s3:// instead");
-    if (config->getStrOption("binary_cache_secret_key_file") != "")
-        printMsg(lvlError, "hydra.conf: binary_cache_secret_key_file is deprecated and ignored. use store_uri=...?secret-key= instead");
 
     createDirs(rootsDir);
 }
