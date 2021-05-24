302f5e0de9a53d6dc73aea90851b54dc3ff29055
Maximilian Bosch <maximilian@mbosch.me>
2021-05-24 14:47:30 +0200

    Set `isSystemUser = true;` for each statically declared user
    This is needed to evaluate the module on 21.05.

--- hydra-module.nix
+++ hydra-module.nix
@@ -204,6 +204,7 @@ in
         group = "hydra";
         createHome = true;
         home = baseDir;
+        isSystemUser = true;
         useDefaultShell = true;
       };
 
@@ -211,12 +212,14 @@ in
       { description = "Hydra queue runner";
         group = "hydra";
         useDefaultShell = true;
+        isSystemUser = true;
         home = "${baseDir}/queue-runner"; # really only to keep SSH happy
       };
 
     users.extraUsers.hydra-www =
       { description = "Hydra web server";
         group = "hydra";
+        isSystemUser = true;
         useDefaultShell = true;
       };
 
