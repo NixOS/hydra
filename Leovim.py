725da5d5bd9b8b0f71095e19457aa2ace737c316
L <91376574+LeoTzahal18@users.noreply.github.com>
2021-09-28 03:27:32 -0300

    Create Leovim.py

---
+++ Leovim.py
@@ -0,0 +1,28 @@
+import geometry from mathplotlib
+import math
+import mathplotlib
+import datascience from sciencebook
+import datetime from clock.sec 
+import linux
+import othermachines from system
+import router_imo from router
+import sys from system
+import sthereos from run
+import saturn from sum
+import python 
+
+.start make file generated(geometry.py)
+  datascience = ('./sciencebook')
+  geometry = ('./mathplotlib')
+  datetime = ('./mundi_with_countdown') - ('./mundi')
+   datetime = clock.sec - watch.time
+    result in secounds :.    ; 
+
+End;
+   
+export to ci
+export to Mu
+export to Union 
+export to ישראל 
+
+import and save this file in leocloud
