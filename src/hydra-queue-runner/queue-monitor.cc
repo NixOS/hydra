f14c583ce5188903f7c9db6f99c8c3fb42c77416
John Ericson <John.Ericson@Obsidian.Systems>
2022-01-20 15:09:21 -0500

    Use `copyClosure` instead of `computeFSClosure` + `copyPaths`
    It is more terse, and in the future it is possible `copyClosure` will
    become more sophisticated.

--- src/hydra-queue-runner/queue-monitor.cc
+++ src/hydra-queue-runner/queue-monitor.cc
@@ -513,9 +513,9 @@ Step::ptr State::createStep(ref<Store> destStore,
                         // FIXME: should copy directly from substituter to destStore.
                     }
 
-                    StorePathSet closure;
-                    localStore->computeFSClosure({*path}, closure);
-                    copyPaths(*localStore, *destStore, closure, NoRepair, CheckSigs, NoSubstitute);
+                    copyClosure(*localStore, *destStore,
+                        StorePathSet { *path },
+                        NoRepair, CheckSigs, NoSubstitute);
 
                     time_t stopTime = time(0);
 
