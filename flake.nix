89e8676d8012670893d6580a1031c7ce434e2e4f
Graham Christensen <graham@grahamc.com>
2022-01-24 11:52:32 -0500

    UUID4Tiny: init

--- flake.nix
+++ flake.nix
@@ -432,6 +432,20 @@
               license = with final.lib.licenses; [ artistic1 gpl1Plus ];
             };
           };
+
+          UUID4Tiny = final.buildPerlPackage {
+            pname = "UUID4-Tiny";
+            version = "0.002";
+            src = final.fetchurl {
+              url = "mirror://cpan/authors/id/C/CV/CVLIBRARY/UUID4-Tiny-0.002.tar.gz";
+              sha256 = "e7535b31e386d432dec7adde214348389e1d5cf753e7ed07f1ae04c4360840cf";
+            };
+            meta = {
+              description = "Cryptographically secure v4 UUIDs for Linux x64";
+              license = with final.lib.licenses; [ artistic1 gpl1Plus ];
+            };
+          };
+
         };
 
         hydra = with final; let
@@ -502,6 +516,7 @@
                 TestPostgreSQL
                 TextDiff
                 TextTable
+                UUID4Tiny
                 XMLSimple
                 YAML
               ];
