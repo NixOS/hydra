d0b6329aa88c10434fbf39bebf747c2e25125906
Cole Helbling <cole.helbling@determinate.systems>
2022-01-31 11:44:00 -0800

    sql/upgrade-81: remove unnecessary comment

--- src/sql/upgrade-81.sql
+++ src/sql/upgrade-81.sql
@@ -2,5 +2,3 @@ alter table runcommandlogs add column uuid uuid;
 update runcommandlogs set uuid = gen_random_uuid() where uuid is null;
 alter table runcommandlogs alter column uuid set not null;
 alter table runcommandlogs add constraint RunCommandLogs_uuid_unique unique(uuid);
-# the unique uuid constraint adds a unique, btree index, so we don't need to create out own:
-#    "runcommandlogs_uuid_unique" UNIQUE CONSTRAINT, btree (uuid)
