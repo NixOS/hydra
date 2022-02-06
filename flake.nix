5ae26aa7604f714dcc73edcb74fe71ddc8957f6c
Maximilian Bosch <maximilian@mbosch.me>
2022-02-06 15:05:15 +0100

    Update Nix to 2.6

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "A Nix-based continuous build system";
 
   inputs.nixpkgs.follows = "nix/nixpkgs";
+  inputs.nix.url = github:NixOS/nix/2.6.0;
 
   outputs = { self, nixpkgs, nix }:
     let
