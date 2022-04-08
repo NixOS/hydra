87b1cad47ab61ce23fa0cb0dafc51d706dc754eb
Graham Christensen <graham@grahamc.com>
2022-04-08 10:35:05 -0400

    PromMetrics:  fix indentation

--- src/hydra-queue-runner/metrics.hh
+++ src/hydra-queue-runner/metrics.hh
@@ -23,21 +23,21 @@ public:
 };
 
 
-    struct PromMetrics
-    {
-        std::shared_ptr<prometheus::Registry> registry;
+struct PromMetrics
+{
+    std::shared_ptr<prometheus::Registry> registry;
 
-        prometheus::Counter& queue_checks_started;
-        prometheus::Histogram& queue_build_fetch_time;
-        prometheus::Family<prometheus::Histogram>& queue_build_load_family;
-        prometheus::Histogram& queue_build_load_premature_gc;
-        prometheus::Histogram& queue_build_load_cached_failure;
-        prometheus::Histogram& queue_build_load_cached_success;
-        prometheus::Histogram& queue_build_load_added;
-        prometheus::Counter& queue_steps_created;
-        prometheus::Counter& queue_checks_early_exits;
-        prometheus::Counter& queue_checks_finished;
-        prometheus::Gauge& queue_max_id;
+    prometheus::Counter& queue_checks_started;
+    prometheus::Histogram& queue_build_fetch_time;
+    prometheus::Family<prometheus::Histogram>& queue_build_load_family;
+    prometheus::Histogram& queue_build_load_premature_gc;
+    prometheus::Histogram& queue_build_load_cached_failure;
+    prometheus::Histogram& queue_build_load_cached_success;
+    prometheus::Histogram& queue_build_load_added;
+    prometheus::Counter& queue_steps_created;
+    prometheus::Counter& queue_checks_early_exits;
+    prometheus::Counter& queue_checks_finished;
+    prometheus::Gauge& queue_max_id;
 
-        PromMetrics();
-    };
+    PromMetrics();
+};
