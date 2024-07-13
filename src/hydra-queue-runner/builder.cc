c3b6e7b425325b2ea65a0ed638c5913ac32d1a28
Pierre Bourdon <delroth@gmail.com>
2024-07-13 06:12:35 +0200

    queue runner: fix nullptr deref on build exception after releasing a machine reservation

--- src/hydra-queue-runner/builder.cc
+++ src/hydra-queue-runner/builder.cc
@@ -37,11 +37,12 @@ void State::builder(MachineReservation::ptr reservation)
 
         try {
             auto destStore = getDestStore();
+            // Might release the reservation.
             res = doBuildStep(destStore, reservation, activeStep);
         } catch (std::exception & e) {
             printMsg(lvlError, "uncaught exception building ‘%s’ on ‘%s’: %s",
-                localStore->printStorePath(reservation->step->drvPath),
-                reservation->machine->sshName,
+                localStore->printStorePath(activeStep->step->drvPath),
+                reservation ? reservation->machine->sshName : std::string("(no machine)"),
                 e.what());
         }
     }
