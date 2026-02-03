a13bca4c2d3bd28e54d904c44572d6ee683b0791
KJ Tsanaktsidis <kj@kjtsanaktsidis.id.au>
2026-02-03 05:08:40 +0000

    Add test for OIDC implementation
    fixup! Add test for OIDC implementation
    
    Move test-only Perl modules (TestWWWMechanize, HTTPCookieJar, ...) from
    the main hydra package to hydra-tests where they belong.

--- subprojects/hydra-tests/package.nix
+++ subprojects/hydra-tests/package.nix
@@ -11,6 +11,7 @@
   ninja,
 
   perl,
+  perlPackages,
   nixComponents,
 
   bzip2,
@@ -87,7 +88,14 @@ stdenv.mkDerivation (finalAttrs: {
     nixComponents.nix-cli
     hydra.perlDeps
     perl
-  ];
+  ]
+  ++ (with perlPackages; [
+    # Test-only Perl modules used by the OIDC end-to-end test
+    HTTPCookieJar
+    TestLongString
+    TestWWWMechanize
+    TestWWWMechanizeCatalyst
+  ]);
 
   OPENLDAP_ROOT = openldap;
 
