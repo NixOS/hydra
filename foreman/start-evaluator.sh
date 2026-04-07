453aa30d0fc6c6c6913cd8b16f1cf507ee402730
Artemis Tosini <artemis.tosini@obsidian.systems>
2026-04-07 12:25:55 -0400

    Fix foreman development
    It was broken since moving hydra to subprojects. Doesn't entirely
    fix static files in hydra, but basic functionality works for testing.

--- foreman/start-evaluator.sh
+++ foreman/start-evaluator.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-export PATH=$(pwd)/src/script:$PATH
+export PATH=$(pwd)/build/subprojects/hydra/hydra-evaluator:$PATH
+
+export HYDRA_DBI="dbi:Pg:dbname=hydra;host=localhost;port=64444"
 
 # wait for postgresql and hydra-server
 while ! pg_isready -h $(pwd)/.hydra-data/postgres -p 64444; do sleep 1; done
