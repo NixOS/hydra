5b57904a46df1d47bb3047b28a218efc41a01d90
John Ericson <John.Ericson@Obsidian.Systems>
2026-05-06 15:48:04 -0400

    Update `harmonia`

--- subprojects/hydra-builder/package.nix
+++ subprojects/hydra-builder/package.nix
@@ -34,7 +34,7 @@ rustPlatform.buildRustPackage {
   cargoLock = {
     lockFile = ../../Cargo.lock;
     outputHashes = {
-      "harmonia-store-core-0.0.0-alpha.0" = "sha256-T6Mbhet2sNGqU9wT5keCAKCSJKrDJ1NuuvtmWp7XUPY=";
+      "harmonia-store-core-0.0.0-alpha.0" = "sha256-HfbltKVSZB+eg8CIPvT9/+EWRtHBomYA8kmAquG9gSw=";
     };
   };
 
