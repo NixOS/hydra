289ad861c38ef9ecee50b385f3d36da6bc1e49ce
John Ericson <John.Ericson@Obsidian.Systems>
2026-05-12 12:45:34 -0400

    Cargo update

--- cargo-output-hashes.nix
+++ cargo-output-hashes.nix
@@ -1,5 +1,5 @@
 # Shared outputHashes for cargoLock across all Rust crates.
 # Update this file when the harmonia dependency changes.
 {
-  "harmonia-store-core-0.0.0-alpha.0" = "sha256-gGmAdjripiNRAM2u/cBH/DMmwct3H2R36NwVxL3nbW0=";
+  "harmonia-store-core-0.0.0-alpha.0" = "sha256-QvaHv5kJMPOEs4UpFWYHqrlgIIZQwBnWfhWiWjzhhoo=";
 }
